a67e12d35e2d1338654fc0be0764252f6d0c0ce2
Lexter Tapawan <lexter.tapawan@gmail.com>
2023-11-15 20:19:21 +0000

    Initial Commit

---
+++ Notes Watch App/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes Watch App
+//
+//  Created by Lexter Tapawan on 15/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Notes_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
